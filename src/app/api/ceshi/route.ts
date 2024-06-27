23332c5ad8c244560b4540bc69de83cb492644f7
pengxinseo <208015671@qq.com>
2024-06-27 15:58:27 +0800

    第二次测试

--- src/app/api/ceshi/route.ts
+++ src/app/api/ceshi/route.ts
@@ -1,7 +1,7 @@
 import {demo} from "@/servers/Tool";
-
+export const runtime = 'edge';
 export async function POST(req: Request, res: Response) {
-    
+
   const result = await demo();
   return Response.json(result);
 }
